03789fdd437658ac220ae6e8890f97022a97426b
jogyemin <jogyemin5100@gmail.com>
2026-01-13 20:54:27 +0900

    Player.cs

--- 콘솔프로젝트/Accidentally a Game/Player.cs
+++ 콘솔프로젝트/Accidentally a Game/Player.cs
@@ -3,21 +3,17 @@ namespace TurnBattleGame
 
     class Player
     {
-
         public int HP = 100;
-
         public int MaxHP = 100;
-
+        
         public int NormalAtk = 15;
-
         public int SkillAtk = 35;
-
+        
         public int SkillCooldown = 0;
-
         public int MaxSkillCooldown = 5;
 
         public int Potion = 5;
-
+        
         public void UseSkill()
         {
             SkillCooldown = MaxSkillCooldown;
